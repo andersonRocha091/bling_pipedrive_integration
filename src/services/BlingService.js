1067c8ace6d96ae800adb20efd21177498ced8b7
Anderson Rocha <anderson.opensev@gmail.com>
2020-08-09 12:57:22 -0300

    Generating basic revenue creating xml with only required fields for registering an revenue into bling

--- src/services/BlingService.js
+++ src/services/BlingService.js
@@ -3,6 +3,22 @@ const xmlBuilder = require("xmlbuilder");
 
 class BlingService {
   constructor() {}
+
+  createXml(value, type = "U", name) {
+    let revenue = {
+      contareceber: {
+        valor: value,
+        ocorrencia: {
+          ocorrenciaTipo: type,
+        },
+        cliente: {
+          nome: name.length > 120 ? name.substring(0, 119) : name,
+        },
+      },
+    };
+    var xml = xmlBuilder.create(revenue).end({ pretty: true });
+    return xml;
+  }
 }
 
 module.exports = BlingService;
--- src/tests/BlingService.test.js
+++ src/tests/BlingService.test.js
@@ -1,3 +1,25 @@
 const assert = require("assert");
 
-describe("Testing bling integration routines", function () {});
+const BlingService = require("../services/BlingService");
+
+const MOCK_XML_REVENUE_CREATE = `<?xml version="1.0"?>
+<contareceber>
+  <valor>200</valor>
+  <ocorrencia>
+    <ocorrenciaTipo>U</ocorrenciaTipo>
+  </ocorrencia>
+  <cliente>
+    <nome>Sergio Fernandes</nome>
+  </cliente>
+</contareceber>`;
+
+describe("Testing bling integration routines", function () {
+  this.beforeAll(() => {
+    blingService = new BlingService();
+  });
+
+  it("Generating xml to be sent in a request", () => {
+    const xmlString = blingService.createXml(200, "U", "Sergio Fernandes");
+    assert.equal(xmlString, MOCK_XML_REVENUE_CREATE);
+  });
+});
