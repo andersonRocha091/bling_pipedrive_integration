72726764c1280e908af74239d7ebbe5f3f259177
Anderson Rocha <anderson.opensev@gmail.com>
2020-08-09 11:22:53 -0300

    Installing XML dependencies, and setting up
    BlingService Test for TDD

---
+++ src/services/BlingService.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+const xmlBuilder = require("xmlbuilder");
+
+class BlingService {
+  constructor() {}
+}
+
+module.exports = BlingService;
---
+++ src/tests/BlingService.test.js
@@ -0,0 +1,3 @@
+const assert = require("assert");
+
+describe("Testing bling integration routines", function () {});
